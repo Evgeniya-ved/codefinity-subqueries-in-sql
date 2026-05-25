facc580da42fa75754a356f941c687e4f9eedee3
Evgeniya-ved <>
2026-05-25 18:12:18 +0000

    Challenge: Top Customers by Order Count

--- subqueries_in_e_commerce_analytics/challenge_top_customers_by_order_count/query.sql
+++ subqueries_in_e_commerce_analytics/challenge_top_customers_by_order_count/query.sql
@@ -1,10 +1,20 @@
-SELECT 
-    C.customer_id, 
-    C.name,
-  (SELECT COUNT(*)
-    FROM ORDERS o
-    WHERE O.CUSTOMER_ID=C.customer_id)  
-    AS ORDER_COUNT
-FROM customers C
- ORDER BY C.customer_id
+SELECT
+    c.customer_id,
+    c.name,
+    customer_order_counts.order_count
+FROM
+    customers c
+    JOIN (
+        SELECT
+            customer_id,
+            COUNT(*) AS order_count
+        FROM
+            orders
+        GROUP BY
+            customer_id
+    ) AS customer_order_counts
+        ON c.customer_id = customer_order_counts.customer_id
+ORDER BY
+    customer_order_counts.order_count DESC,
+    c.customer_id ASC
 LIMIT 3;
